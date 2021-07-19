406662cef89285059ed0317cc4e4ec9d85886073
Michael Mussato <michimussato@gmail.com>
2021-07-19 22:47:07 +0200

    - transmission redirect

--- jukeoroni/urls.py
+++ jukeoroni/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('radio/', include('radio.urls')),
     path('player/', include('player.urls')),
+    path('transmission', include('transmission.urls')),
     path('admin/', admin.site.urls),
 ]
