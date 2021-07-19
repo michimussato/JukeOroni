e49d44edb9bfb6c716c7e991fc2e29b970667713
Michael Mussato <michimussato@gmail.com>
2021-07-19 21:37:31 +0200

    - set logging level to DEBUT

--- jukeoroni/settings.py
+++ jukeoroni/settings.py
@@ -11,17 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import logging.config
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-LOGGING = {'root':
-               {'level': 'DEBUG',
-                'handlers': ['console']
-                }
-           }
+logging.getLogger(__file__).setLevel(logging.DEBUG)
 
 
 # Quick-start development settings - unsuitable for production
