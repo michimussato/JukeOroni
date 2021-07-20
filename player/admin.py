dfebe59bfa7f22954ed23ff93e2fd54efe3b4dfa
Michael Mussato <michimussato@gmail.com>
2021-07-20 13:40:13 +0200

    - cleanup - db for player

--- player/admin.py
+++ player/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Artist, Album, Track
+# from .models import Artist, Album, Track
+from .models import Track
 
-admin.site.register(Artist)
-admin.site.register(Album)
+# admin.site.register(Artist)
+# admin.site.register(Album)
 admin.site.register(Track)
