8447ce99f26c5858511fd2786215c5b07f2f3dca
Michael Mussato <michimussato@gmail.com>
2021-07-20 14:40:43 +0200

    - cleanup - db for player

--- player/models.py
+++ player/models.py
@@ -15,8 +15,8 @@ from django.db import models
 
 class DjangoTrack(models.Model):
     # album = models.ForeignKey(Album, on_delete=models.PROTECT)
-    # track_title = models.CharField(max_length=200, unique=False)
-    audio_source = models.URLField(max_length=200, unique=True, blank=True)
+    track_title = models.CharField(max_length=200, unique=False)
+    # audio_source = models.URLField(max_length=200, unique=True, blank=True)
 
     def __str__(self):
         return self.audio_source
