2f1db99314aa72bdd8767133ca9720276c05e744
Michael Mussato <michimussato@gmail.com>
2021-07-24 04:46:42 +0200

    - font size changed

--- player/player.py
+++ player/player.py
@@ -407,8 +407,9 @@ class Player(object):
 
                 # multiprocessing approach
                 self.loading_process = multiprocessing.Process(target=self._load_track_task, kwargs={'track': next_track})
+                self.loading_process.daemon = True
                 self.loading_process.start()
-                self.loading_process.join()
+                # self.loading_process.join()
 
                 self.loading += 1
                 # thread.start()
