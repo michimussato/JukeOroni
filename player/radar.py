37745df0d83fe3b92f1402f0f5e3c1cbdf53695c
Michael Mussato <michimussato@gmail.com>
2021-07-27 19:35:35 +0200

    clock

--- player/radar.py
+++ player/radar.py
@@ -1,32 +1,22 @@
-import selenium.webdriver
-import selenium.common
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-from PIL import Image, ImageDraw
-from io import BytesIO
-import tempfile
 import os
+import tempfile
+from io import BytesIO
+from PIL import Image, ImageDraw
+import selenium.common
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def radar_screenshot(factor=1.0):
     options = selenium.webdriver.firefox.options.Options()
     options.headless = True
-    # options.log_path = '/tmp/geckodriver.log'
-    # options.service_log_path = '/tmp/geckodriver.log'
     service_log_path = os.path.join(tempfile.gettempdir(), 'geckodriver.log')
     with selenium.webdriver.Firefox(options=options, service_log_path=service_log_path) as driver:
         driver.get('https://meteo.search.ch/prognosis')
-        # TODO: wait for element instead of fixed time
-        time.sleep(4)
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
         root = driver.find_element(By.ID, "mapcontainer")
-        # root = driver.find_element(By.CLASS, "leaflet-pane leaflet-map-pane")
-        # root = driver.find_element_by_class_name("leaflet-pane leaflet-map-pane")
-        # root = driver.find_element_by_tag_name('html')
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span.cbot-btn__switch"))).click()
-        root.screenshot('/home/pi/test_screenshot.png')
         png = root.screenshot_as_png
     im = Image.open(BytesIO(png))
     width, height = im.size
@@ -36,14 +26,7 @@ def radar_screenshot(factor=1.0):
     botton = height - top
     im = im.crop((left, top, right, botton))
 
-    # print(im.size)
-    # print(im.size)
-    # print(im.size)
-    # print(im.size)
-    # print(im.size)
     im = im.resize((int(im.size[0] * factor), int(im.size[1] * factor)))
-
-    # w, h = im.size
 
     bg = Image.new(mode='RGB', size=im.size, color=(0, 0, 0))
     mask = Image.new("L", im.size, 0)
@@ -52,20 +35,3 @@ def radar_screenshot(factor=1.0):
     im = Image.composite(im, bg, mask)
 
     return im
-
-    # if draw_radar:
-    #     radar_bg = Image.new(mode='RGB', size=(448, 152), color=(0, 0, 0))
-    #     # new_bg.paste(bg)
-    #     # bg = new_bg
-    #     # bg_radar = Image.new(mode='RGB', size=(448, 152), color=(0, 0, 0))
-    #     factor = 0.25
-    #     radar = radar_screenshot()
-    #     radar = radar.resize((int(radar.size[0]*factor), int(radar.size[1]*factor)))
-    #     radar_size = radar.size
-    #     radar_bg.paste(radar, (0, 0))
-    #     radar_bg = radar_bg.rotate(90, expand=False)
-    #     # radar_bg = radar_bg.rotate(90, expand=False)
-    #     bg.paste(radar_bg, (0, 0))
-    #     # bg =
-
-# radar_screenshot()
