edd2210808871ef81226e58f1a5f5e1d563391e7
Michael Mussato <michimussato@gmail.com>
2021-07-29 01:04:04 +0200

    clock

--- player/displays.py
+++ player/displays.py
@@ -7,10 +7,10 @@ def buttons_img_overlay(labels):
     buttons_img = Image.new(mode='RGB', size=(448, 12), color=(0, 0, 0))
     buttons_draw = ImageDraw.Draw(buttons_img)
     buttons_draw.text((0, 0), '       {0}               {1}               {2}           {3}'.format(
-        labels[4],
         labels[3],
         labels[2],
         labels[1],
+        labels[0],
     ), fill=(255, 255, 255))
 
     return buttons_img
