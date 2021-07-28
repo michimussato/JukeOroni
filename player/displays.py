b7a37314987c6d64d8d786bdb2bf437f9306d1c2
Michael Mussato <michimussato@gmail.com>
2021-07-29 01:46:12 +0200

    clock

--- player/displays.py
+++ player/displays.py
@@ -4,7 +4,7 @@ from .radar import Radar
 
 
 def buttons_img_overlay(labels):
-    buttons_img = Image.new(mode='RGB', size=(448, 12), color=(0, 0, 0))
+    buttons_img = Image.new(mode='RGB', size=(448, 12), color=(80, 80, 80))
     buttons_draw = ImageDraw.Draw(buttons_img)
     buttons_draw.text((0, 0), '       {0}               {1}               {2}           {3}'.format(
         labels[3],
@@ -13,14 +13,12 @@ def buttons_img_overlay(labels):
         labels[0],
     ), fill=(255, 255, 255))
 
-    buttons_img = buttons_img.rotate(90, expand=True)
-    print(buttons_img.size)
-    return buttons_img
+    return buttons_img.rotate(90, expand=True)
 
 
 def standby(labels):
-    _clock = clock(draw_logo=True, draw_date=True, hours=24, draw_astral=True)
-    print(_clock.size)
+    _clock = clock(size=200, draw_logo=True, draw_date=True, hours=24, draw_astral=True)
+    # print(_clock.size)
     _radar = Radar()
     _radar_image = _radar.image(scaled_by=0.45)
 
