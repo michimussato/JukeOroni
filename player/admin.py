87de8b40bc8ecdd3db4b0aea7c34d22429cf55a5
Michael Mussato <michimussato@gmail.com>
2021-07-29 14:15:27 +0200

    radio

--- player/admin.py
+++ player/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Artist, Album, Track  # , Channel
+from radio.models import Channel
 # from .models import Track
 
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Track)
-# admin.site.register(Channel)
+admin.site.register(Channel)
