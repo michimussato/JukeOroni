a795ef9fb749f5745f1a1ff3ad7e73143ec73f67
Michael Mussato <michimussato@gmail.com>
2021-07-29 15:51:01 +0200

    radio

--- player/views.py
+++ player/views.py
@@ -28,57 +28,57 @@ def index(request):
     return HttpResponse('Player page')
 
 
-def radio_index(request):
-    # channels = Channel.objects.all()
-    #
-    # ret = '<html>\n'
-    # ret += '  <body>\n'
-    # for channel in channels:
-    #     if channel.is_enabled:
-    #         # required to find out if and which channel is playing
-    #         ps = subprocess.Popen(['ps -o cmd -p $(pidof mplayer) | grep -i {0}'.format(channel.url)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #         output = ps.communicate()[0].decode('utf-8').replace('\n', '')
-    #         if output != '':
-    #             pid = subprocess.Popen(['pidof mplayer'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #             pid_output = pid.communicate()[0].decode('utf-8').replace('\n', '')
-    #             ret += f'        <button style=\"width:100%; background-color:green; \" onclick=\"window.location.href = \'stop/{pid_output}\';\">{channel.display_name}</button>\n'  # , channel.display_name)
-    #         else:
-    #             ret += f'        <button style=\"width:100%\" onclick=\"window.location.href = \'{channel.display_name_short}/play\';\">{channel.display_name}</button>\n'
-    # ret += '  </body>\n'
-    # ret += '</html>\n'
-    return HttpResponse(ret)
-
-
-def radio_play(request, display_name_short):
-
-    # # pid = subprocess.Popen(['pidof mplayer'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # # pid_output = pid.communicate()[0].decode('utf-8').replace('\n', '')
-    #
-    # try:
-    #     for c in Channel.objects.filter(last_played=True):
-    #         c.last_played = False
-    #         c.save()
-    # except radio.models.Channel.DoesNotExist:
-    #     pass
-    #
-    # # if pid_output != '':
-    # #     os.system(f'kill {pid_output}')
-    # channel = Channel.objects.get(display_name_short=display_name_short)
-    # # subprocess.Popen(['mplayer', '-nogui', '-noconfig', 'all', '-novideo', '-nocache', '-playlist', channel.url])
-    # channel.last_played = True
-    # channel.save()
-    #
-    # # set_image(image_file=channel.url_logo, media_info=channel.display_name)
-
-    return HttpResponseRedirect('/player/radio')
-
-
-def radio_stop(request, pid):
-    # os.system(f'kill {pid}')
-    # set_image(image_file=SLEEP_IMAGE, media_info='')
-    return HttpResponseRedirect('/player/radio')
-
-
+# def radio_index(request):
+#     channels = Channel.objects.all()
+#
+#     ret = '<html>\n'
+#     ret += '  <body>\n'
+#     for channel in channels:
+#         if channel.is_enabled:
+#             # required to find out if and which channel is playing
+#             ps = subprocess.Popen(['ps -o cmd -p $(pidof mplayer) | grep -i {0}'.format(channel.url)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#             output = ps.communicate()[0].decode('utf-8').replace('\n', '')
+#             if output != '':
+#                 pid = subprocess.Popen(['pidof mplayer'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#                 pid_output = pid.communicate()[0].decode('utf-8').replace('\n', '')
+#                 ret += f'        <button style=\"width:100%; background-color:green; \" onclick=\"window.location.href = \'stop/{pid_output}\';\">{channel.display_name}</button>\n'  # , channel.display_name)
+#             else:
+#                 ret += f'        <button style=\"width:100%\" onclick=\"window.location.href = \'{channel.display_name_short}/play\';\">{channel.display_name}</button>\n'
+#     ret += '  </body>\n'
+#     ret += '</html>\n'
+#     return HttpResponse(ret)
+#
+#
+# def radio_play(request, display_name_short):
+#
+#     pid = subprocess.Popen(['pidof mplayer'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     pid_output = pid.communicate()[0].decode('utf-8').replace('\n', '')
+#
+#     try:
+#         for c in Channel.objects.filter(last_played=True):
+#             c.last_played = False
+#             c.save()
+#     except radio.models.Channel.DoesNotExist:
+#         pass
+#
+#     if pid_output != '':
+#         os.system(f'kill {pid_output}')
+#     channel = Channel.objects.get(display_name_short=display_name_short)
+#     subprocess.Popen(['mplayer', '-nogui', '-noconfig', 'all', '-novideo', '-nocache', '-playlist', channel.url])
+#     channel.last_played = True
+#     channel.save()
+#
+#     set_image(image_file=channel.url_logo, media_info=channel.display_name)
+#
+#     return HttpResponseRedirect('/player/radio')
+#
+#
+# def radio_stop(request, pid):
+#     os.system(f'kill {pid}')
+#     set_image(image_file=SLEEP_IMAGE, media_info='')
+#     return HttpResponseRedirect('/player/radio')
+#
+#
 # def set_image(image_file, media_info):
 #     logging.debug('starting setting image task')
 #     thread = threading.Thread(target=task_pimoroni_set_image, kwargs={'image_file': image_file, 'media_info': media_info})
@@ -174,3 +174,4 @@ def radio_stop(request, pid):
 #
 #     pimoroni.set_image(bg, saturation=PIMORONI_SATURATION)
 #     pimoroni.show()
+#
