cd8c520556135542da82bdf058d9bc9cd277d0df
Michael Mussato <michimussato@gmail.com>
2021-07-31 21:02:24 +0200

    refactor

--- player/player.py
+++ player/player.py
@@ -364,11 +364,13 @@ class Player(object):
     def _track_loader_task(self):
         while True and not self._quit:
             print('here')
+            print(len(self.tracks) + int(bool(self.loading_process)) < MAX_CACHED_FILES)
+            print(not bool(self.loading_process))
             if len(self.tracks) + int(bool(self.loading_process)) < MAX_CACHED_FILES and not bool(self.loading_process):
                 print('and here')
                 next_track = self.get_next_track()
                 if next_track is None:
-                    # print('stuck here?')
+                    print('stuck here?')
                     time.sleep(1.0)
                     continue
 
