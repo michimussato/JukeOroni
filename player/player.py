c44e712d78b9b029435f49b8a2f6dd44f2df9a5a
Michael Mussato <michimussato@gmail.com>
2021-07-31 18:30:22 +0200

    refactor

--- player/player.py
+++ player/player.py
@@ -620,7 +620,9 @@ class Player(object):
         print('killing self.loading_process and resetting it to None')
         if self.loading_process is not None:
             self.loading_process.terminate()
-            # self.loading_process.join()
+            # a process can be joined multiple times:
+            # here: just wait for termination before proceeding
+            self.loading_process.join()
         self.loading_process = None
         # remove all cached tracks from the filesystem except the one
         # that is currently playing
