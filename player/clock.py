d0ceec444d6fb018340110e4f1b2224b781029f8
Michael Mussato <michimussato@gmail.com>
2021-08-06 23:02:18 +0200

    rgba test

--- player/clock.py
+++ player/clock.py
@@ -23,9 +23,13 @@ class Clock:
         assert hours in [12, 24], 'hours can only be 12 or 24'
 
         # bg = Image.new(mode='RGB', size=(_size, _size), color=(0, 0, 0, 0))
-        bg = Image.new(mode='RGB', size=(_size, _size))
-        mask_bg_draw = ImageDraw.Draw(bg, 'RGBA')
+        _bg = Image.new(mode='RGBA', size=(_size, _size), color=(255, 0, 0))
+        alpha_mask = Image.new('L', _bg.size, 0)
+        alpha_mask_draw = ImageDraw.Draw(alpha_mask)
+        alpha_mask_draw.ellipse([(0, 0), (300, 300)], fill=255)
+        mask_bg_draw = ImageDraw.Draw(_bg, 'RGBA')
         mask_bg_draw.ellipse([(0,0),(300,300)], fill=(255,0,0,255))
+        bg = Image.composite(_bg, _bg, mask_bg_draw)
         # image = Image.new(mode='RGB', size=(_size, _size), color=(0, 0, 0))
         # draw = ImageDraw.Draw(image)
 
