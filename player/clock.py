25121f98ef85035678d7cc075e4089cb30d0de85
Michael Mussato <michimussato@gmail.com>
2021-08-06 23:33:09 +0200

    rgba test

--- player/clock.py
+++ player/clock.py
@@ -26,7 +26,7 @@ class Clock:
         bg = Image.new(mode='RGBA', size=(_size, _size), color=(255, 255, 255, 0))
         # _bg = Image.new(mode='RGBA', size=(_size, _size))
         draw = ImageDraw.Draw(bg)
-        draw.ellipse([(0, 0), (300, 300)], fill=(0,0,0,255))
+        draw.ellipse([(0, 0), (300, 300)], fill=(0,255,0,255))
         # bg = Image.alpha_composite(bg, _bg)
         # draw = Image.Imag
         # alpha_mask = Image.new('L', _bg.size, 0)
