2c7e3947e5ce01ca5c6540482a0e5cdb7e90a5e0
Michael Mussato <michimussato@gmail.com>
2021-08-07 09:25:20 +0200

    rgba test

--- player/displays.py
+++ player/displays.py
@@ -5,12 +5,12 @@ from .radar import Radar
 
 def buttons_img_overlay(labels, stby=False):
     bg = Image.new(mode='RGBA', size=(600, 448), color=(0, 0, 0, 0))
-    # widget_buttons = Image.new(mode='RGBA', size=(448, 16), color=(255, 255, 255, 128))
+    widget_buttons = Image.new(mode='RGBA', size=(448, 16), color=(255, 255, 255, 128))
     comp_buttons = Image.new(mode='RGBA', size=(448, 16))
 
-    comp_buttons = Image.alpha_composite(comp_buttons, bg)
+    comp_buttons = Image.alpha_composite(comp_buttons, widget_buttons)
 
-    # bg.paste(comp_buttons)
+    bg.paste(comp_buttons, mask=comp_buttons)
 
     # # bg = Image.new(mode='RGBA', size=(448, 16), color=(80, 80, 80, 0))
     # # buttons_img = Image.new(mode='RGBA', size=(448, 16), color=(80, 80, 80, 64))
