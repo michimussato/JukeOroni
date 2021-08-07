1115fe6f8671de38d3db06d7023ac0fa7bd469fd
Michael Mussato <michimussato@gmail.com>
2021-08-07 23:44:32 +0200

    rgba test

--- player/player.py
+++ player/player.py
@@ -458,7 +458,8 @@ class Player(object):
 
                 self.loading_process.join()
                 print('5')
-                ret = self.loading_queue.get()
+                if self.loading_process is not None:
+                    ret = self.loading_queue.get()
                 print('6')
                 # print(f'ret: {ret}')
                 # print(f'ret 2: {ret}')
