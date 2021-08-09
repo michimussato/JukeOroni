8ccfdd27af3298742d2f37d39d4c11682f6633b5
Michael Mussato <michimussato@gmail.com>
2021-08-09 23:01:02 +0200

    class based views test

--- player/urls.py
+++ player/urls.py
@@ -5,7 +5,7 @@ from player.views import PlayerView
 
 urlpatterns = [
     path('', PlayerView.as_view(), name='player_index'),
-    path('play/', PlayerView.play(), name='player_play'),
+    path('play/', PlayerView.play, name='player_play'),
     path('radio/', views.radio_index, name='radio_index'),
     path('radio/stop/<int:pid>/', views.radio_stop, name='radio_stop'),
     path('radio/<str:display_name_short>/play/', views.radio_play, name='radio_play')
