2ab5be905c6086f3f6cd27d0e37f3ae45dd9a50b
Michael Mussato <michimussato@gmail.com>
2021-08-19 03:45:52 +0200

    continue work on jukeoroni

--- jukeoroni/settings.py
+++ jukeoroni/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from jukeoroni.custom_secrets import DJANGO_SECRET_KEY
+from jukeoroni._secrets import DJANGO_SECRET_KEY
 from pathlib import Path
 
 
