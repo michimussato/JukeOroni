ecafaff10315c99309e6863286ecc2925067e9bb
Michael Mussato <michimussato@gmail.com>
2021-10-11 23:20:09 +0200

    continue work on jukeoroni

--- player/jukeoroni/clock.py
+++ player/jukeoroni/clock.py
@@ -50,34 +50,17 @@ class Clock:
             arc_twelve = 270.0
 
         white = (255, 255, 255, 255)
-        black = (0, 0, 0, 64)
+        black = (0, 0, 0, 0)
         toggle = {white: black, black: white}
 
-        if draw_astral:
-            city = LocationInfo('Bern', 'Switzerland', tz, 46.94809, 7.44744)
-            _sun = sun(city.observer, date=datetime.date.today(), tzinfo=city.timezone)
 
-            decimal_sunrise = float(_sun['sunrise'].strftime('%H')) + float(_sun['sunrise'].strftime('%M')) / 60
-            arc_length_sunrise = decimal_sunrise / hours * 360.0
-            LOG.info(f'Sunrise: {str(_sun["sunrise"].strftime("%H:%M"))}')
-
-            decimal_sunset = float(_sun['sunset'].strftime('%H')) + float(_sun['sunset'].strftime('%M')) / 60
-            arc_length_sunset = decimal_sunset / hours * 360.0
-            LOG.info(f'Sunset: {str(_sun["sunset"].strftime("%H:%M"))}')
-
-            color = (255, 128, 0, 255)
-            _size_astral = 0.17  # TODO: bigger means smaller circle
-            _width = 0.012
-            size_astral = [(round(_size * _size_astral), round(_size * _size_astral)), (round(_size - _size * _size_astral), round(_size - _size * _size_astral))]
-            width_astral = round(_size * _width)
-            draw.arc(size_astral, start=arc_length_sunrise+arc_twelve, end=arc_length_sunset+arc_twelve, fill=color,
-                     width=width_astral)
 
         LOG.info(f'Moon phase: {round(float(astral.moon.phase()))} / 28')
         if draw_moon:
-            fill = (255, 0, 0, 255)
-            _moon = Image.new(mode='RGBA', size=(_size, _size), color=(0, 0, 0, 0))
-            _draw_moon = ImageDraw.Draw(_moon)
+            fill = (170, 0, 0, 255)
+            # _moon = Image.new(mode='RGBA', size=(_size, _size), color=(0, 0, 0, 0))
+            # _draw_moon = ImageDraw.Draw(_moon)
+            _draw_moon = draw
             _draw_moon.ellipse((0, 0, _size, _size), fill=fill)
             phase = round(float(astral.moon.phase()) / 28.0 * 2, 4)
 
@@ -111,57 +94,77 @@ class Clock:
                 _draw_moon.ellipse((center - (spherical * center), 0, center + (spherical * center), _size),
                              fill=(0, 0, 0, 0))
 
+        if draw_astral:
+            city = LocationInfo('Bern', 'Switzerland', tz, 46.94809, 7.44744)
+            _sun = sun(city.observer, date=datetime.date.today(), tzinfo=city.timezone)
+
+            decimal_sunrise = float(_sun['sunrise'].strftime('%H')) + float(_sun['sunrise'].strftime('%M')) / 60
+            arc_length_sunrise = decimal_sunrise / hours * 360.0
+            LOG.info(f'Sunrise: {str(_sun["sunrise"].strftime("%H:%M"))}')
+
+            decimal_sunset = float(_sun['sunset'].strftime('%H')) + float(_sun['sunset'].strftime('%M')) / 60
+            arc_length_sunset = decimal_sunset / hours * 360.0
+            LOG.info(f'Sunset: {str(_sun["sunset"].strftime("%H:%M"))}')
+
+            color = (255, 128, 0, 255)
+            _size_astral = 0.17  # TODO: bigger means smaller circle
+            _width = 0.012
+            size_astral = [(round(_size * _size_astral), round(_size * _size_astral)), (round(_size - _size * _size_astral), round(_size - _size * _size_astral))]
+            width_astral = round(_size * _width)
+            draw.arc(size_astral, start=arc_length_sunrise+arc_twelve, end=arc_length_sunset+arc_twelve, fill=color,
+                     width=width_astral)
+
         # center dot
         draw.ellipse([(round(_size * 0.482), round(_size * 0.482)), (round(_size - _size * 0.482), round(_size - _size * 0.482))], fill=white, outline=None, width=round(_size * 0.312))
 
         color = white
         # TODO: we could do the intervals smarter now
         if hours == 24:
-            intervals = [0.0, 3.0,
-                         14.0, 16.0,
-                         29.0, 31.0,
-                         42.0, 48.0,
-                         59.0, 61.0,
-                         74.0, 76.0,
-                         87.0, 93.0,
-                         104.0, 106.0,
-                         119.0, 121.0,
-                         132.0, 138.0,
-                         149.0, 151.0,
-                         164.0, 166.0,
-                         177.0, 183.0,
-                         194.0, 196.0,
-                         209.0, 211.0,
-                         222.0, 228.0,
-                         239.0, 241.0,
-                         254.0, 256.0,
-                         267.0, 273.0,
-                         284.0, 286.0,
-                         299.0, 301.0,
-                         312.0, 318.0,
-                         329.0, 331.0,
-                         344.0, 346.0,
-                         357.0, 359.99,
+            intervals = [(0.0, 3.0),
+                         (14.0, 16.0),
+                         (29.0, 31.0),
+                         (42.0, 48.0),
+                         (59.0, 61.0),
+                         (74.0, 76.0),
+                         (87.0, 93.0),
+                         (104.0, 106.0),
+                         (119.0, 121.0),
+                         (132.0, 138.0),
+                         (149.0, 151.0),
+                         (164.0, 166.0),
+                         (177.0, 183.0),
+                         (194.0, 196.0),
+                         (209.0, 211.0),
+                         (222.0, 228.0),
+                         (239.0, 241.0),
+                         (254.0, 256.0),
+                         (267.0, 273.0),
+                         (284.0, 286.0),
+                         (299.0, 301.0),
+                         (312.0, 318.0),
+                         (329.0, 331.0),
+                         (344.0, 346.0),
+                         (357.0, 359.99),
                          ]
         else:
-            intervals = [0.0, 3.0,
-                         29.0, 31.0,
-                         59.0, 61.0,
-                         87.0, 93.0,
-                         119.0, 121.0,
-                         149.0, 151.0,
-                         177.0, 183.0,
-                         209.0, 211.0,
-                         239.0, 241.0,
-                         267.0, 273.0,
-                         299.0, 301.0,
-                         329.0, 331.0,
-                         357.0, 359.99,
+            intervals = [(0.0, 3.0),
+                         (29.0, 31.0),
+                         (59.0, 61.0),
+                         (87.0, 93.0),
+                         (119.0, 121.0),
+                         (149.0, 151.0),
+                         (177.0, 183.0),
+                         (209.0, 211.0),
+                         (239.0, 241.0),
+                         (267.0, 273.0),
+                         (299.0, 301.0),
+                         (329.0, 331.0),
+                         (357.0, 359.99),
                          ]
 
-        for interval in intervals[::-1]:  # reversed
-            draw.arc([(round(_size * 0.022), round(_size * 0.022)), (round(_size - _size * 0.022), round(_size - _size * 0.022))], start=arc_twelve, end=(arc_twelve + interval) % 360, fill=color, width=round(_size * 0.060))
-            color = toggle[color]
+        for start, end in intervals[::-1]:  # reversed
+            draw.arc([(round(_size * 0.022), round(_size * 0.022)), (round(_size - _size * 0.022), round(_size - _size * 0.022))], start=start, end=end, fill=color, width=round(_size * 0.060))
+            # color = toggle[color]
 
         decimal_h = float(datetime.datetime.now().strftime('%H')) + float(datetime.datetime.now().strftime('%M')) / 60
         arc_length_h = decimal_h / hours * 360.0
@@ -188,8 +191,8 @@ class Clock:
         comp = Image.new(mode='RGBA', size=(_size, _size))
         comp = Image.alpha_composite(comp, bg)
         comp = Image.alpha_composite(comp, _clock)
-        if draw_moon:
-            comp = Image.alpha_composite(comp, _moon)
+        # if draw_moon:
+        #     comp = Image.alpha_composite(comp, _moon)
         comp = comp.rotate(90, expand=False)
 
         comp = comp.resize((round(_size/ANTIALIAS), round(_size/ANTIALIAS)), Image.ANTIALIAS)
