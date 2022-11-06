a4dfed9f00c44e1bf2359ad3b88d093595f6983c
Michael Mussato <michimussato@gmail.com>
2022-11-06 23:28:37 +0100

    clear channels to prep file

--- ansible/prep.sh
+++ ansible/prep.sh
@@ -12,5 +12,7 @@ python manage.py createsuperuser
 
 # write initial radio channels
 python manage.py shell
+# >>> from player.init_db.manual_db_routines import remove_channels
+# >>> remove_channels()
 # >>> from player.init_db.manual_db_routines import channels_write_table
 # >>> channels_write_table()
